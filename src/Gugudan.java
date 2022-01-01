f6f583d5137240685a4b7deaad7a1788c088f972
hokyun <hgk9872@gmail.com>
2022-01-02 00:44:42 +0900

    initial commit

---
+++ src/Gugudan.java
@@ -0,0 +1,17 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		// 2단 
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		
+		// 3단
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+	}
+}
